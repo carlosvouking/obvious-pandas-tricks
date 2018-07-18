6522b42a0c6fc52f0174806d56948a9272ae918f
Carlos Vouking <carlosvouking@gmail.com>
2018-07-18 14:03:21 +0200

    Update pandas-tricks.py

--- pandas-tricks.py
+++ pandas-tricks.py
@@ -1,4 +1,4 @@
-1/ show all dataframe columns
+### 1/ show all dataframe columns
     pd.set_option('display.max_columns', None)
-2/ show n columns from the dataframe
+### 2/ show n columns from the dataframe
     pd.set_option('display.max_columns', None)
